82ebbffecd982659cc4d92cb5ffaa75bb30f1760
leynnnnnn0 <nathanielalvarez1234569@gmail.com>
2024-07-19 21:15:02 +0800

    added leave options

---
+++ public/animation/sidebar.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const showLeaveOptionsButton = document.getElementById("leaveOptions");
+    showLeaveOptionsButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        document.getElementById("leaveOptionsContainer").classList.toggle("hidden");
+    })
+});
--- view/partial/sidebar.php
+++ view/partial/sidebar.php
@@ -9,8 +9,20 @@
     <div class="flex items-center h-14 font-semibold px-5 text-gray-800 cursor-pointer <?= Application::$application->style->sideBarButton('/employee') ?>">
         <a href="/employee"><span class="mr-3"><i class="bi bi-person"></i></span> Employee</a>
     </div>
-    <div class="flex items-center h-14 font-semibold px-5 text-gray-800 cursor-pointer <?= Application::$application->style->sideBarButton('/leave') ?>">
+    <div class="flex gap-4 h-14 items-center justify-between font-semibold px-5 text-gray-800 cursor-pointer <?= Application::$application->style->sideBarButton('/leave') ?>">
         <a href="/leave"><span class="mr-3"><i class="bi bi-box-arrow-left"></i></span> Leave</a>
+        <span id="leaveOptions" class="mr-3"><i class="bi bi-chevron-down"></i></span>
+    </div>
+    <div id="leaveOptionsContainer" class="container bg-gray-200">
+        <div class="flex rounded-lg h-10 items-center text-sm font-semibold px-5 text-gray-800 cursor-pointer <?= Application::$application->style->sideBarButton('/leave') ?>">
+            <a href="/leave">Summary</a>
+        </div>
+        <div class="flex h-10 items-center text-sm font-semibold px-5 text-gray-800 cursor-pointer">
+            <a href="/leave">Status</a>
+        </div>
+        <div class="flex h-10 items-center text-sm font-semibold px-5 text-gray-800 cursor-pointer">
+            <a href="/leave">Request</a>
+        </div>
     </div>
     <div class="flex items-center h-14 font-semibold px-5 text-gray-800 cursor-pointer <?= Application::$application->style->sideBarButton('/attendance') ?>">
         <span class="mr-3"><i class="bi bi-clock"></i></span> Attendance
@@ -22,3 +34,4 @@
         <span class="mr-3"><i class="bi bi-gear"></i></span> Setting
     </div>
 </div>
+<script src="/animation/sidebar.js"></script>
